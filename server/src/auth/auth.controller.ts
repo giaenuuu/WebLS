b779ad42b611c6c7f93fc702fff64b1b05339c8e
giaenuuu <admin@gruee.ch>
2024-01-08 14:27:47 +0100

    add logout & finalize session

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -10,6 +10,15 @@ export class AuthController {
   @Post('login')
   async login(@Request() req, @Response() res): Promise<void> {
     const userInput = req.body;
+
+    if (req.session && req.session.user) {
+      // Return the existing session without creating a new one
+      res
+        .status(200)
+        .json({ message: 'Session already exists', user: req.session.user });
+      return;
+    }
+
     const user = await this.authService.authenticateUser(userInput);
     if (user) {
       // Generate a unique session token
@@ -28,5 +37,14 @@ export class AuthController {
   public async register(@Body() userInput: UserInput): Promise<void> {
     this.authService.createUser(userInput);
     return;
+  }
+
+  @Post('logout')
+  async logout(@Request() req, @Response() res): Promise<void> {
+    req.session = null;
+    // Clear session cookie on the client
+    res.clearCookie('webls_session');
+
+    res.status(200).json({ message: 'Logout successful' });
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session'
+import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
 import * as cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   // app.use(cookieParser(), session({
   //   name: 'WEBLS_SESSION',
   //   secret: 'TODO-PLEASE_CHANGE_LOCATION_OF_TOKEN',
@@ -18,14 +18,17 @@ async function bootstrap() {
   //     httpOnly: true,
   //   }
   // }))
-  app.use(cookieParser('TODO_PLEASE_CHANGE_LOCATION_OF_TOKEN'),cookieSession({
-    name: 'TODO_PLEASE_CHANGE_LOCATION_OF_TOKEN',
-    keys: ['your-secret-key'],
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-    httpOnly: true,
-    secure: false, // Set to true in production if using HTTPS
-    signed: true,
-  }));
+  app.use(
+    cookieParser('TODO_PLEASE_CHANGE_LOCATION_OF_TOKEN'),
+    cookieSession({
+      name: 'webls_session',
+      keys: ['TODO_PLEASE_CHANGE_LOCATION_OF_TOKEN'],
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      httpOnly: true,
+      secure: false, // Set to true in production if using HTTPS
+      signed: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
